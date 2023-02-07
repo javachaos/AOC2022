8445f62e1fdd667cc1ef96ff8cdbd1a63cba175a
Alfred Laderoute <alfred_e@outlook.com>
2023-02-07 14:35:36 -0500

    Simplified Main

--- src/main/java/com/github/javachaos/aoc2022/Main.java
+++ src/main/java/com/github/javachaos/aoc2022/Main.java
@@ -3,7 +3,6 @@ package com.github.javachaos.aoc2022;
 import com.github.javachaos.aoc2022.problems.Day01p1;
 import com.github.javachaos.aoc2022.problems.Day01p2;
 import com.github.javachaos.aoc2022.problems.IProblem;
-import com.github.javachaos.aoc2022.problems.Problem;
 
 import java.util.ArrayList;
 
@@ -11,12 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<IProblem> problems = new ArrayList<>();
-        Problem d1 = new Day01p1();
-        Problem d1_2 = new Day01p2();
 
         //Add problem here
-        problems.add(d1);
-        problems.add(d1_2);
+        problems.add(new Day01p1());
+        problems.add(new Day01p2());
 
         //RUN THROUGH ALL PROBLEMS AND EXECUTE THEM!!!
         problems.forEach(IProblem::executeProblem);
